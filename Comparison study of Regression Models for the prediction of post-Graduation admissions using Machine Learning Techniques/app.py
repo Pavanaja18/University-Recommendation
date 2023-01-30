2b097fe6ab59911284e5ca23d4773d9724ba330f
Pavanaja18 <68804682+Pavanaja18@users.noreply.github.com>
2023-01-30 21:53:11 +0530

    Add files via upload

---
+++ Comparison study of Regression Models for the prediction of post-Graduation admissions using Machine Learning Techniques/app.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('rf_model_sc.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['GET','post'])
+def predict():
+	
+	GRE_Score = int(request.form['GRE Score'])
+	TOEFL_Score = int(request.form['TOEFL Score'])
+	University_Rating = int(request.form['University Rating'])
+	SOP = float(request.form['SOP'])
+	LOR = float(request.form['LOR'])
+	CGPA = float(request.form['CGPA'])
+	Research = int(request.form['Research'])
+	
+	final_features = pd.DataFrame([[GRE_Score, TOEFL_Score, University_Rating, SOP, LOR, CGPA, Research]])
+	
+	predict = model.predict(final_features)
+	
+	output = predict[0]
+	
+	return render_template('index.html', prediction_text='Admission chances are {}'.format(output))
+
+@app.route('/page1.html')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page5.html')
+def page5():
+    return render_template('page5.html')
+
+@app.route('/page4.html')
+def page4():
+    return render_template('page4.html')
+
+@app.route('/page3.html')
+def page3():
+    return render_template('page3.html')
+
+@app.route('/page2.html')
+def page2():
+    return render_template('page2.html')
+
+
+	
+if __name__ == "__main__":
+	app.run(debug=True)
